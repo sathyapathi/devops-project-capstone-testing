79b4c4a8a3d4a9a0df5415a5c9ecb9e1fb4a4391
Sathya <sathyapathi@gmail.comm>
2025-03-02 19:02:40 -0500

    Re-uploading local repository

---
+++ src/main/java/com/insureme/App.java
@@ -0,0 +1,13 @@
+package com.insureme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
